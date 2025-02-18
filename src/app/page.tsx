ac0f9f1ff1bee6e0bc9a13a80d0730813e4a92c3
Fernando Araoz <fernando@araozu.dev>
2025-02-18 14:44:31 -0500

    feat: add shadcnui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,6 @@ import Image from "next/image";
 
 export default function Home()
 {
-    if (Math.random() < 0.5)
-    {
-        console.log("...");
-    }
     return (
         <div
             className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20"
