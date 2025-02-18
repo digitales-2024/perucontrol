83377b0d6ec0233e3bd6ffcf81ff512bb74b7a71
Fernando Araoz <fernando@araozu.dev>
2025-02-18 12:14:46 -0500

    fix: static files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Atkinson_Hyperlegible } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-    variable: "--font-geist-sans",
-    subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
-    subsets: ["latin"],
+const atkinsonHyperlegible = Atkinson_Hyperlegible({
+    variable: "--font-atkinson",
+    weight: ["400", "700"]
 });
 
 export const metadata: Metadata = {
@@ -20,12 +15,12 @@ export const metadata: Metadata = {
 export default function RootLayout({
     children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
+        <html lang="es">
             <body
-                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+                className={`${atkinsonHyperlegible.variable} antialiased`}
             >
                 {children}
             </body>
