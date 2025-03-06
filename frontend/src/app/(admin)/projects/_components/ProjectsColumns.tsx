63b15f7dce9cfff58debf5ac520cb30e3e750f38
Victor-Salazar-Infantes <victormsi.x20@gmail.com>
2025-03-06 15:34:17 -0500

    feat: implement project management components and data table

---
+++ frontend/src/app/(admin)/projects/_components/ProjectsColumns.tsx
@@ -0,0 +1,198 @@
+"use client";
+
+import type { ColumnDef } from "@tanstack/react-table";
+import { ArrowDown, ArrowUp, ArrowUpDown, Ellipsis } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem } from "@/components/ui/dropdown-menu";
+import { DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
+import { components } from "@/types/api";
+
+export const columns: Array<ColumnDef<components["schemas"]["Project"]>> = [
+    {
+        accessorKey: "orderNumber",
+        header: ({ column }) => (
+            <Button
+                variant="ghost"
+                onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+                className="p-0 hover:bg-transparent"
+            >
+              # Orden
+                {column.getIsSorted() === "asc" ? (
+                    <ArrowUp className="ml-1 h-4 w-4" />
+                ) : column.getIsSorted() === "desc" ? (
+                    <ArrowDown className="ml-1 h-4 w-4" />
+                ) : (
+                    <ArrowUpDown className="ml-1 h-4 w-4" />
+                )}
+            </Button>
+        ),
+        cell: ({ row }) => (
+            <span className="items-center flex justify-center uppercase text-center">
+                {row.original.orderNumber}
+            </span>
+        ),
+    },
+    {
+        accessorKey: "client",
+        header: ({ column }) => (
+            <Button
+                variant="ghost"
+                onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+                className="p-0 hover:bg-transparent"
+            >
+              Cliente
+                {column.getIsSorted() === "asc" ? (
+                    <ArrowUp className="ml-1 h-4 w-4" />
+                ) : column.getIsSorted() === "desc" ? (
+                    <ArrowDown className="ml-1 h-4 w-4" />
+                ) : (
+                    <ArrowUpDown className="ml-1 h-4 w-4" />
+                )}
+            </Button>
+        ),
+        cell: ({ row }) => (
+            <span className="items-center flex justify-center text-center">
+                {row.original.client}
+            </span>
+        ),
+    },
+    {
+        accessorKey: "state",
+        header: ({ column }) => (
+            <Button
+                variant="ghost"
+                onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+                className="p-0 hover:bg-transparent"
+            >
+                Estado
+                {column.getIsSorted() === "asc" ? (
+                    <ArrowUp className="ml-1 h-4 w-4" />
+                ) : column.getIsSorted() === "desc" ? (
+                    <ArrowDown className="ml-1 h-4 w-4" />
+                ) : (
+                    <ArrowUpDown className="ml-1 h-4 w-4" />
+                )}
+            </Button>
+        ),
+        cell: ({ row }) => (
+            <span className="flex justify-center">
+                {row.original.state}
+            </span>
+        ),
+    },
+    {
+        accessorKey: "area",
+        header: ({ column }) => (
+            <Button
+                variant="ghost"
+                onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+                className="p-0 hover:bg-transparent"
+            >
+              Área m2
+                {column.getIsSorted() === "asc" ? (
+                    <ArrowUp className="ml-1 h-4 w-4" />
+                ) : column.getIsSorted() === "desc" ? (
+                    <ArrowDown className="ml-1 h-4 w-4" />
+                ) : (
+                    <ArrowUpDown className="ml-1 h-4 w-4" />
+                )}
+            </Button>
+        ),
+        cell: ({ row }) => (
+            <span className="flex justify-center">
+                {row.original.area}
+            </span>
+        ),
+    },
+    {
+        accessorKey: "spacesCount",
+        header: ({ column }) => (
+            <Button
+                variant="ghost"
+                onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+                className="p-0 hover:bg-transparent"
+            >
+            Nro. de Ambientes
+                {column.getIsSorted() === "asc" ? (
+                    <ArrowUp className="ml-1 h-4 w-4" />
+                ) : column.getIsSorted() === "desc" ? (
+                    <ArrowDown className="ml-1 h-4 w-4" />
+                ) : (
+                    <ArrowUpDown className="ml-1 h-4 w-4" />
+                )}
+            </Button>
+        ),
+        cell: ({ row }) => (
+            <span className="items-center text-center flex justify-center">
+                {row.original.spacesCount}
+            </span>
+        ),
+    },
+    {
+        accessorKey: "address",
+        header: ({ column }) => (
+            <Button
+                variant="ghost"
+                onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+                className="p-0 hover:bg-transparent"
+            >
+              Dirección
+                {column.getIsSorted() === "asc" ? (
+                    <ArrowUp className="ml-1 h-4 w-4" />
+                ) : column.getIsSorted() === "desc" ? (
+                    <ArrowDown className="ml-1 h-4 w-4" />
+                ) : (
+                    <ArrowUpDown className="ml-1 h-4 w-4" />
+                )}
+            </Button>
+        ),
+        cell: ({ row }) => (
+            <span className="items-center text-center flex justify-center">
+                {row.original.address}
+            </span>
+        ),
+    },
+    {
+        id: "acciones",
+        header: "Acciones",
+        cell: function Cell()
+        {
+            // const [showUpdateQuotation, setShowUpdateQuotation] = useState(false);
+            /* const [showDeleteQuotation, setShowDeleteQuotation] = useState(false);
+            const [showDetailQuotation, setShowDetailQuotation] = useState(false); */
+
+            return (
+                <div>
+                    <div>
+                        {/* Acciones */}
+                    </div>
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Button
+                                aria-label="Open menu"
+                                variant="ghost"
+                                className="flex size-8 p-0 data-[state=open]:bg-muted"
+                            >
+                                <Ellipsis
+                                    className="size-4"
+                                    aria-hidden="true"
+                                />
+                            </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent align="end">
+                            <DropdownMenuItem>
+                                Ver
+                            </DropdownMenuItem>
+                            <DropdownMenuItem>
+                                Editar
+                            </DropdownMenuItem>
+                            <DropdownMenuItem>
+                                Eliminar
+                            </DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                </div>
+            );
+        },
+    },
+];
---
+++ frontend/src/app/(admin)/projects/page.tsx
@@ -0,0 +1,23 @@
+import { HeaderPage } from "@/components/common/HeaderPage";
+import { Shell } from "@/components/common/Shell";
+import { ProjectsDataTable } from "./_components/ProjectsDataTable";
+import { columns } from "./_components/ProjectsColumns";
+import { backend, wrapper } from "@/types/backend";
+
+export default async function ProjectsPage()
+{
+    // Get all projects
+    const [projects, error] = await wrapper((auth) => backend.GET("/api/Project", auth));
+    if (error)
+    {
+        console.error(`error ${error.message}`);
+        throw error;
+    }
+
+    return (
+        <Shell>
+            <HeaderPage title="Gestión de Servicios" description="Gestiona los servicios de tu empresa" />
+            <ProjectsDataTable columns={columns} data={projects} />
+        </Shell>
+    );
+}
