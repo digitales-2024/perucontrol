86058afcc6ef92c30c32d8f83d57d7413c376e8a
Fernando Araoz <fernando@araozu.dev>
2025-03-24 17:06:36 -0500

    feat: integrate charlietlamb/calendar

---
+++ frontend/src/app/components/CalendarDemo.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { useState } from "react";
+import Calendar from "./calendar/calendar";
+import { CalendarEvent, Mode } from "./calendar/calendar-types";
+
+export default function CalendarDemo()
+{
+    const [events, setEvents] = useState<Array<CalendarEvent>>([]);
+    const [mode, setMode] = useState<Mode>("month");
+    const [date, setDate] = useState<Date>(new Date());
+
+    return (
+        <Calendar
+            events={events}
+            setEvents={setEvents}
+            mode={mode}
+            setMode={setMode}
+            date={date}
+            setDate={setDate}
+        />
+    );
+}
