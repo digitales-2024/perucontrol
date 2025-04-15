0f50c451a11adf312844b504ac36a6b12230d615
Fernando Araoz <fernando@araozu.dev>
2025-04-15 09:50:12 -0500

    feat: optimize data loading on server components

--- frontend/src/app/(admin)/cotizaciones/[id]/update/page.tsx
+++ frontend/src/app/(admin)/cotizaciones/[id]/update/page.tsx
@@ -4,45 +4,48 @@ import EditQuotation from "./_components/_EditQuotation";
 export default async function EditarCotizacionPage({ params }: { params: Promise<{ id: string }> })
 {
     const quotationId = (await params).id;
-
     if (!quotationId)
     {
         throw new Error("Quotation ID is required");
     }
 
-    // get the quotation to edit
-    const [quotation, quotationErr] = await wrapper((auth) => backend.GET("/api/Quotation/{id}", {
-        ...auth,
-        params: {
-            path: {
-                id: quotationId,
+    const [
+        [quotation, quotationErr],
+        [terms, termsErr],
+        [clients, clientsError],
+        [services, servicesError],
+    ] = await Promise.all([
+        wrapper((auth) => backend.GET("/api/Quotation/{id}", {
+            ...auth,
+            params: {
+                path: {
+                    id: quotationId,
+                },
             },
-        },
-    }));
+        })),
+        wrapper((auth) => backend.GET("/api/TermsAndConditions", auth)),
+        wrapper((auth) => backend.GET("/api/Client", auth)),
+        wrapper((auth) => backend.GET("/api/Service", { ...auth })),
+    ]);
+
     if (quotationErr)
     {
         console.error(`Error fetching quotation: ${quotationErr.message}`);
         throw quotationErr;
     }
 
-    // get all terms and conditions
-    const [terms, termsErr] = await wrapper((auth) => backend.GET("/api/TermsAndConditions", auth));
     if (termsErr)
     {
         console.error(`Error fetching terms: ${termsErr.message}`);
         throw termsErr;
     }
 
-    // get all clients
-    const [clients, clientsError] = await wrapper((auth) => backend.GET("/api/Client", { ...auth }));
     if (clientsError)
     {
         console.error("Error getting all clients:", clientsError);
         throw clientsError;
     }
 
-    // get all services
-    const [services, servicesError] = await wrapper((auth) => backend.GET("/api/Service", { ...auth }));
     if (servicesError)
     {
         console.error("Error getting all services:", servicesError);
