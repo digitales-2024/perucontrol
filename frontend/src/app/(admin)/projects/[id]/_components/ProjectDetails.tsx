df58527947ea936f731221514904d55eaacb4c58
Victor-Salazar-Infantes <victormsi.x20@gmail.com>
2025-05-08 14:35:36 -0500

    feat: agregada visualizacion resumida de areas y productos tratados por appointment

--- frontend/src/app/(admin)/projects/[id]/_components/ProjectDetails.tsx
+++ frontend/src/app/(admin)/projects/[id]/_components/ProjectDetails.tsx
@@ -46,6 +46,8 @@ export type AppointmentForTable = {
     cancelled?: boolean/*  | null | undefined */;
     enterTime?: string | null | undefined;
     leaveTime?: string | null | undefined;
+    treatmentAreas?: Array<components["schemas"]["TreatmentAreaGetDTO"]>;
+    treatmentProducts?: Array<components["schemas"]["TreatmentProductDTO"]>;
 };
 
 function getServiceIcon(name: string): string
