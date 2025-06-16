c16f673f10315064315dfe777a4e50674ac925d8
Fernando Araoz <fernando@araozu.dev>
2025-06-16 16:48:58 -0500

    refactor: move operationsheet endpoints

--- backend/src/Controllers/Appointment/AppointmentController.cs
+++ backend/src/Controllers/Appointment/AppointmentController.cs
@@ -470,68 +470,6 @@ public class AppointmentController(
         return Ok();
     }
 
-    [EndpointSummary("Update an operation sheet")]
-    [HttpPatch("{appointmentid}/operation-sheet")]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    public async Task<ActionResult<ProjectOperationSheet>> UpdateOperationSheet(
-        Guid appointmentid,
-        [FromBody] OperationSheetPatchDTO updateDTO
-    )
-    {
-        var operationSheet = await db.Set<ProjectOperationSheet>()
-            .Include(x => x.ProjectAppointment)
-            .FirstOrDefaultAsync(x => x.ProjectAppointment.Id == appointmentid);
-
-        if (operationSheet == null)
-        {
-            return NotFound("No se encontró una ficha de operaciones para la fecha especificada.");
-        }
-
-        if (updateDTO.EnterTime is not null)
-        {
-            operationSheet.ProjectAppointment.EnterTime = updateDTO.EnterTime;
-        }
-        if (updateDTO.LeaveTime is not null)
-        {
-            operationSheet.ProjectAppointment.LeaveTime = updateDTO.LeaveTime;
-        }
-        if (operationSheet.Status == OperationSheetStatus.Created)
-            operationSheet.Status = OperationSheetStatus.Started;
-
-        // Aplicar los cambios al objeto existente
-        updateDTO.ApplyPatch(operationSheet);
-
-        // Guardar los cambios en la base de datos
-        db.Update(operationSheet);
-        await db.SaveChangesAsync();
-
-        return Ok(operationSheet);
-    }
-
-    [EndpointSummary("Find operation sheet by project ID")]
-    [HttpGet("operation-sheet/by-project/{projectId}")]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public async Task<ActionResult<ProjectOperationSheet>> FindByIdProject(Guid projectId)
-    {
-        // Buscar la ficha operativa asociada al ProjectAppointment del proyecto
-        var operationSheet = await db.Set<ProjectOperationSheet>()
-            .Include(x => x.ProjectAppointment) // Incluir la relación con ProjectAppointment
-            .ThenInclude(pa => pa.Project) // Incluir la relación con el Project
-            .FirstOrDefaultAsync(x => x.ProjectAppointment.Project.Id == projectId);
-
-        if (operationSheet == null)
-        {
-            return NotFound(
-                "No se encontró una ficha de operaciones para el proyecto especificado."
-            );
-        }
-
-        return Ok(operationSheet);
-    }
-
     [EndpointSummary("Update a certificate")]
     [HttpPatch("{appointmentid}/certificate")]
     [ProducesResponseType(StatusCodes.Status200OK)]
--- backend/src/Controllers/OperationsSheet/OperationSheetController.cs
+++ backend/src/Controllers/OperationsSheet/OperationSheetController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using PeruControl.Infrastructure.Model;
 
 namespace PeruControl.Controllers;
@@ -70,5 +71,67 @@ public class OperationSheetController(
     {
         await operationSheetService.MarkOperationSheetCreated(operationSheetId);
         return Ok();
+    }
+
+    [EndpointSummary("Update an operation sheet by appointment id")]
+    [HttpPatch("by-appointment/{appointmentid}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<ProjectOperationSheet>> UpdateOperationSheetByAppointment(
+        Guid appointmentid,
+        [FromBody] OperationSheetPatchDTO updateDTO
+    )
+    {
+        var operationSheet = await _dbSet
+            .Include(x => x.ProjectAppointment)
+            .FirstOrDefaultAsync(x => x.ProjectAppointment.Id == appointmentid);
+
+        if (operationSheet == null)
+        {
+            return NotFound("No se encontró una ficha de operaciones para la fecha especificada.");
+        }
+
+        if (updateDTO.EnterTime is not null)
+        {
+            operationSheet.ProjectAppointment.EnterTime = updateDTO.EnterTime;
+        }
+        if (updateDTO.LeaveTime is not null)
+        {
+            operationSheet.ProjectAppointment.LeaveTime = updateDTO.LeaveTime;
+        }
+        if (operationSheet.Status == OperationSheetStatus.Created)
+            operationSheet.Status = OperationSheetStatus.Started;
+
+        // Aplicar los cambios al objeto existente
+        updateDTO.ApplyPatch(operationSheet);
+
+        // Guardar los cambios en la base de datos
+        _context.Update(operationSheet);
+        await _context.SaveChangesAsync();
+
+        return Ok(operationSheet);
+    }
+
+    [EndpointSummary("Find operation sheet by project ID")]
+    [HttpGet("by-project/{projectId}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<ProjectOperationSheet>> FindByProjectId(Guid projectId)
+    {
+        // Buscar la ficha operativa asociada al ProjectAppointment del proyecto
+        var operationSheet = await _dbSet
+            .Include(x => x.ProjectAppointment) // Incluir la relación con ProjectAppointment
+            .ThenInclude(pa => pa.Project) // Incluir la relación con el Project
+            .FirstOrDefaultAsync(x => x.ProjectAppointment.Project.Id == projectId);
+
+        if (operationSheet == null)
+        {
+            return NotFound(
+                "No se encontró una ficha de operaciones para el proyecto especificado."
+            );
+        }
+
+        return Ok(operationSheet);
     }
 }
